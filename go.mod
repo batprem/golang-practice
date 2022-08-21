29268fa2e467a00a3910b8bad71a565ac2527cdd
prem <prem.ch@ku.th>
2022-08-21 18:42:03 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/batprem/golang-practice
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/batprem/golang-practice/woosha"
+)
+
+func main() {
+	fmt.Println(woosah.Greet2())
+}
